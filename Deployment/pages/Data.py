2d95aad2f0f2dcb6366bc62b7dc3375e4844ecb2
Mohamed Talaat <147813555+mohamedtalaat91@users.noreply.github.com>
2024-09-05 00:15:45 +0300

    Update Data.py

--- Deployment/pages/Data.py
+++ Deployment/pages/Data.py
@@ -5,11 +5,11 @@ from zipfile import ZipFile
 
 
 
-zip_file_path = r"E:\Epsilon AI\final_project\Deployment\source\churn.csv"
+zip_file_path = "Deployment/source/churn.csv"
 data = pd.read_csv(zip_file_path)
 
 st.markdown('''<center> <h1> Customer Churn Data Discription </h1> </center>''',unsafe_allow_html=True)
-disc = pd.read_csv(r"E:\Epsilon AI\final_project\Deployment\source\description.csv" ,index_col='column')
+disc = pd.read_csv("Deployment/source/description.csv" ,index_col='column')
 st.write(disc)
 st.markdown('''<center> <h1> Customer Churn Data </h1> </center>''',unsafe_allow_html=True)
 st.markdown('<a href ="https://www.kaggle.com/datasets/ankitverma2010/ecommerce-customer-churn-analysis-and-prediction/data"> <center> <h2> Link </h2> </center> ', unsafe_allow_html=True)
